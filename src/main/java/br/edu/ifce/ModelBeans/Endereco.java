0495fe170556f2093de0eaee48a811c7e2161cd6
JB <j.batista.damasceno@icloud.com>
2021-02-16 19:30:39 -0300

    Criado o banco de dados e os beans respectivos

---
+++ src/main/java/br/edu/ifce/ModelBeans/Endereco.java
@@ -0,0 +1,47 @@
+package br.edu.ifce.ModelBeans;
+
+public class Endereco {
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private int cep;
+	private String pais;
+	
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public int getCep() {
+		return cep;
+	}
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+}	
---
+++ src/main/java/br/edu/ifce/Utils/DbUtil.java
@@ -0,0 +1,30 @@
+package br.edu.ifce.Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtil {
+	static Connection connection;
+	
+	public static Connection getConnection() {
+	if (connection != null) {
+		return connection;
+	}else {
+	String url = "jdbc:mysql://localhost:3306/";
+	String dbName ="tjwdb"; 
+	String uname = "admin";
+	String pwd = "adminTWJ2020";
+	
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		connection = DriverManager.getConnection(url+dbName,uname,pwd);
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	return connection;
+	}
+	}
+}

1974a8842132f49c13ec324213f3d1e2ddd8c9de
JB <j.batista.damasceno@icloud.com>
2021-02-17 21:45:10 -0300

    adicionado layout tela home

--- src/main/java/br/edu/ifce/ModelBeans/Categoria.java
+++ src/main/java/br/edu/ifce/ModelBeans/Categoria.java
@@ -3,7 +3,16 @@ package br.edu.ifce.ModelBeans;
 public class Categoria {
 	private String name;
 	private int catgoriaID;
+	private String status;
 	
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 	public int getCatgoriaID() {
 		return catgoriaID;
 	}
